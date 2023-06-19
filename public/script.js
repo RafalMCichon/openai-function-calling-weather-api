26de4e3b8f1bb56404527321f9c61a268315c82f
Your Name <you@your-domain.com>
2023-06-19 13:28:44 -0400

    udpate to ES6

--- public/script.js
+++ public/script.js
@@ -1,31 +1,31 @@
-var socket = io();
+const socket = io();
 
 // Function to create a new list item
-function createListItem(text) {
-    var li = document.createElement('li');
+const createListItem = (text) => {
+    const li = document.createElement('li');
     li.innerHTML = text.replace(/\n/g, '<br>'); // Replace newline characters with <br> tags
     return li;
-}
+};
 
 // Function to create a loading animation
-function createLoadingItem() {
-    var li = document.createElement('li');
+const createLoadingItem = () => {
+    const li = document.createElement('li');
     li.id = 'loading';
-    var span1 = document.createElement('span');
+    const span1 = document.createElement('span');
     span1.className = 'dot';
-    var span2 = document.createElement('span');
+    const span2 = document.createElement('span');
     span2.className = 'dot';
-    var span3 = document.createElement('span');
+    const span3 = document.createElement('span');
     span3.className = 'dot';
     li.appendChild(span1);
     li.appendChild(span2);
     li.appendChild(span3);
     return li;
-}
+};
 
 // When a message is received, remove the loading animation and append the message to the list
-socket.on('bot message', function (msg) {
-    var loading = document.getElementById('loading');
+socket.on('bot message', (msg) => {
+    const loading = document.getElementById('loading');
     if (loading) {
         document.getElementById('messages').removeChild(loading);
     }
@@ -33,10 +33,10 @@ socket.on('bot message', function (msg) {
 });
 
 // When the form is submitted, send the input value
-document.getElementById('message-form').addEventListener('submit', function (e) {
+document.getElementById('message-form').addEventListener('submit', (e) => {
     e.preventDefault(); // Prevent the form from being submitted normally
-    var input = document.getElementById('message-input');
-    var message = input.value;
+    const input = document.getElementById('message-input');
+    const message = input.value;
     socket.emit('chat message', message);
     // Append the user's message and a loading animation to the list
     document.getElementById('messages').appendChild(createListItem(`You: ${message}`));
@@ -46,6 +46,6 @@ document.getElementById('message-form').addEventListener('submit', function (e) 
 });
 
 // Automatically focus on the input field when the page is loaded
-window.addEventListener('load', function () {
+window.addEventListener('load', () => {
     document.getElementById('message-input').focus();
 });
